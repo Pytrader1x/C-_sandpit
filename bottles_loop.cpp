abc6d96bb4751f6416f49c21c89bca2aeb4cc20c
pytrader1x <willukmail@gmail.com>
2020-11-28 18:27:19 +0000

    Updated bottles_loop with a conditional

--- bottles_loop.cpp
+++ bottles_loop.cpp
@@ -5,13 +5,24 @@ using namespace std;
 int main() {
 
   // Write a for loop here:
+
+  int bottles;
+  std::cout << "How many bottles of pop would you like?: \n";
+  std::cin >> bottles;
+
   
-  for (int i = 99 ; i >0; i--)
+  for (int i = bottles ; i >0; i--)
   {
 
-    cout << i << "bottles of pop on the wall." << endl;
+    cout << i << " bottles of pop on the wall." << endl;
     cout << "Take one down and pass it around." << endl;
-    cout << i -1 << "bottles of pop on the wall." << endl;
+    cout << i -1 << " bottles of pop on the wall." << endl;
+
+    if(i == 1){
+      std::cout << i -1<< " No more bottles of pop on the wall \n";
+    }
+
+
 
 
   }
